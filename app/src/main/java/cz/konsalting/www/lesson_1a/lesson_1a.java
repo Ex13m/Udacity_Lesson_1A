721c86c7672bb50b4ae58b6018fe8200a80edea6
Igor Novak <ex333m@gmail.com>
2017-02-12 15:50:26 +0100

    TextView practic end

--- app/src/main/java/cz/konsalting/www/lesson_1a/lesson_1a.java
+++ app/src/main/java/cz/konsalting/www/lesson_1a/lesson_1a.java
@@ -2,12 +2,21 @@ package cz.konsalting.www.lesson_1a;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class lesson_1a extends AppCompatActivity {
+public class lesson_1a extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_1a);
+    }
+
+
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
