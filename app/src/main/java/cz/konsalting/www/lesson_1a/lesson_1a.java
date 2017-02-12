0e5b63e48c97fe231b7ae712bd1e1a74f3f21a34
Igor Novak <ex333m@gmail.com>
2017-02-12 15:32:21 +0100

    Initial commit

---
+++ app/src/main/java/cz/konsalting/www/lesson_1a/lesson_1a.java
@@ -0,0 +1,13 @@
+package cz.konsalting.www.lesson_1a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lesson_1a extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_1a);
+    }
+}
